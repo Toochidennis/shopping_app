1cb1cacad433fd15ca612812accef82ac86412c8
Toochidennis <dennistoochukwu@gmail.com>
2024-03-04 17:45:32 +0100

    New commit

---
+++ android/app/src/main/kotlin/com/toochi/shopping_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.toochi.shopping_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(252, 206, 1, 1),
+          primary:  const Color.fromRGBO(252, 206, 1, 1),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1)
+        )
+      ),
+      title: 'Shopping App',
+      home: const HomePage(),
+    );
+  }
+}
